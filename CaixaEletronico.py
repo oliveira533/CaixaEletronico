36d8f9f7d614ce75737a973147a5733c7ffc02a0
Gustavo de Oliveira <go609127@gmail.com>
2022-05-15 14:16:28 -0300

    Update CaixaEletronico.py
    Adicionei um laço que fica se repetindo até o usuário decidir sair do programa.

--- CaixaEletronico.py
+++ CaixaEletronico.py
@@ -33,49 +33,77 @@ nAbestecimento = 0
 
 # variável que contém o que vai ser feito no caixa
 # feito pelo Gustavo
-nFuncao = int(input("Digite 1 para  sacar \nDigite 2 para depositar \nDigite 3 para fazer login como administrador: \n"))
+nFuncao = int(input("Digite 0 para sair \nDigite 1 para  sacar \nDigite 2 para depositar \nDigite 3 para fazer login como administrador \n"))
 
-# teste para saber o que vai ser feito no caixa
-# feito pelo Gustavo
-if nFuncao == 1:
+# laço de repetição para saber quantas vezes vai ser sacado dinheiro
+# feito pelo gustavo
+while nFuncao != 0:
 
-    # bloco de código que oferece q meno quantidade de notas possível
-    # feito em conjunto pelo grupo
-    nSaque = int(input("Informe o valor que você quer sacar: "))
-    while nSaque >= 200:
-        nSaqueDuzentos += 1
-        nSaque = nSaque - 200
-    while nSaque >= 100:
-        nSaqueCem += 1
-        nSaque = nSaque - 100
-    while nSaque >= 50:
-        nSaqueCinquenta += 1
-        nSaque = nSaque - 50
-    while nSaque >= 20:
-        nSaqueVinte += 1
-        nSaque = nSaque - 20
-    while nSaque >= 10:
-        nSaqueDez += 1
-        nSaque = nSaque - 10
-    while nSaque >= 5:
-        nSaqueCinco += 1
-        nSaque = nSaque - 5
-    while nSaque >= 2:
-        nSaqueDois += 1
-        nSaque = nSaque - 2
-    
-    print('\nNotas de duzentos a receber:', nSaqueDuzentos, 
-    '\nNotas de cem a receber:', nSaqueCem, 
-    '\nNotas de cinquenta a receber:', nSaqueCinquenta, 
-    '\nNotas de vinte a receber:', nSaqueVinte, 
-    '\nNotas de dez a receber: ', nSaqueDez, 
-    '\nNotas de cinco a receber:', nSaqueCinco, 
-    '\nNotas de dois a receber', nSaqueDois )
+    # teste para saber o que vai ser feito no caixa
+    # feito pelo Gustavo
+    if nFuncao == 1:
 
-elif nFuncao == 2:
-    nDeposito = int(input("Informe a quantidade que você quer depositar: "))
-elif nFuncao == 3:
-    sLogin = input("Informe o o login: ")
-    sSenha = input("Informe a senha: ")
-else:
-    print("Escolha uma função:")
+        # bloco de código que oferece q meno quantidade de notas possível
+        # feito em conjunto pelo grupo
+        nSaque = int(input("Informe o valor que você quer sacar: "))
+        while nSaque >= 200:
+            nSaqueDuzentos += 1
+            nSaque = nSaque - 200
+            nQuantidadeCedulas = nQuantidadeCedulas - 200
+            nDuzentos = nDuzentos - 200
+
+        while nSaque >= 100:
+            nSaqueCem += 1
+            nSaque = nSaque - 100
+            nQuantidadeCedulas = nQuantidadeCedulas - 100
+            nCem = nCem - 100
+
+        while nSaque >= 50:
+            nSaqueCinquenta += 1
+            nSaque = nSaque - 50
+            nQuantidadeCedulas = nQuantidadeCedulas - 50
+            nCinquenta = nCinquenta - 50
+
+        while nSaque >= 20:
+            nSaqueVinte += 1
+            nSaque = nSaque - 20
+            nQuantidadeCedulas = nQuantidadeCedulas - 20
+            nVinte =nVinte - 20
+
+        while nSaque >= 10:
+            nSaqueDez += 1
+            nSaque = nSaque - 10
+            nQuantidadeCedulas = nQuantidadeCedulas - 10
+            nDez = nDez - 10
+
+        while nSaque >= 5:
+            nSaqueCinco += 1
+            nSaque = nSaque - 5
+            nQuantidadeCedulas = nQuantidadeCedulas - 5
+            nCinco = nCinco - 5
+
+        while nSaque >= 2:
+            nSaqueDois += 1
+            nSaque = nSaque - 2
+            nQuantidadeCedulas = nQuantidadeCedulas - 2
+            nDois = nDois - 2
+
+        if nSaque == 1:
+            print("\nNão conseguiremos entregar o valor pedido, por isso receberá o valor aproximado")
+        
+        print('\nNotas de duzentos a receber:', nSaqueDuzentos, 
+        '\nNotas de cem a receber:', nSaqueCem, 
+        '\nNotas de cinquenta a receber:', nSaqueCinquenta, 
+        '\nNotas de vinte a receber:', nSaqueVinte, 
+        '\nNotas de dez a receber: ', nSaqueDez, 
+        '\nNotas de cinco a receber:', nSaqueCinco, 
+        '\nNotas de dois a receber', nSaqueDois, "\n")
+
+    elif nFuncao == 2:
+        nDeposito = int(input("Informe a quantidade que você quer depositar: "))
+    elif nFuncao == 3:
+        sLogin = input("Informe o o login: ")
+        sSenha = input("Informe a senha: ")
+    else:
+        print("Escolha uma função:")
+    nFuncao = int(input("Digite 1 para  sacar \nDigite 2 para depositar \nDigite 3 para fazer login como administrador: \n"))
